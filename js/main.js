a3e0bf3dc02a8520fd905330ca6c515634260695
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-14 18:07:46 +1000

    update the bump chart

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
 vegaEmbed('#vis1', 'specs/01_choropleth.json', {actions: false});
-vegaEmbed('#vis2', 'specs/02_bump.json', {actions: false});
+vegaEmbed('#vis2', 'specs/02_bump.json', {actions: false, renderer: 'svg'});
