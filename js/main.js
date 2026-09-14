97f6aacddeb94f7b7608d82aa6e24639ed7729dc
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-14 18:27:18 +1000

    add third chart connected scatterplot

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 vegaEmbed('#vis1', 'specs/01_choropleth.json', {actions: false});
 vegaEmbed('#vis2', 'specs/02_bump.json', {actions: false, renderer: 'svg'});
+vegaEmbed('#vis3', 'specs/03_connected.json', {actions: false, renderer: 'svg'});
