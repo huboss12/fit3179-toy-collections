a71ae8e779b2366e5a3d0ca210560961fe19c8ed
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-15 16:24:26 +1000

    add flow map to the ass 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 vegaEmbed('#vis1', 'specs/01_choropleth.json', {actions: false});
 vegaEmbed('#vis2', 'specs/02_bump.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis3', 'specs/03_connected.json', {actions: false, renderer: 'svg'});
+vegaEmbed('#vis4', 'specs/04_flowmap.json', {actions: false, renderer: 'svg'});
