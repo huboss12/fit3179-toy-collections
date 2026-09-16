287d72ba3be0b34ce72fd424f0192e913a959700
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-16 15:20:49 +1000

    add chart 6 to page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ vegaEmbed('#vis2', 'specs/02_bump.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis3', 'specs/03_connected.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis4', 'specs/04_flowmap.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis5', 'specs/05_streamgraph.json', {actions: false, renderer: 'svg'});
+vegaEmbed('#vis6', 'specs/06_symbolmap.json', {actions: false, renderer: 'svg'});
