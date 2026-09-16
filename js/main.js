f3076602e77e2768f334747e50a87a9d67a62ed6
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-16 15:46:20 +1000

    add chart 7 heat map to the page

--- js/main.js
+++ js/main.js
@@ -4,3 +4,4 @@ vegaEmbed('#vis3', 'specs/03_connected.json', {actions: false, renderer: 'svg'})
 vegaEmbed('#vis4', 'specs/04_flowmap.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis5', 'specs/05_streamgraph.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis6', 'specs/06_symbolmap.json', {actions: false, renderer: 'svg'});
+vegaEmbed('#vis7', 'specs/07_heatmap.json', {actions: false, renderer: 'svg'});
