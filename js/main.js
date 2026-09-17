931b915faab1f24e048f3df0fd0db44566b77cd8
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-17 14:25:21 +1000

    add spike map for page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,4 @@ vegaEmbed('#vis4', 'specs/04_flowmap.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis5', 'specs/05_streamgraph.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis6', 'specs/06_symbolmap.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis7', 'specs/07_heatmap.json', {actions: false, renderer: 'svg'});
+vegaEmbed('#vis8', 'specs/08_spikemap.json', {actions: false, renderer: 'svg'});
