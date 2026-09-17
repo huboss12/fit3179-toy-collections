15e9d8d5fd2794183cbc26163df6b052a1e64c06
Jarrod Li <jarrodlizeji@gmail.com>
2026-09-17 16:52:21 +1000

    add ridgeline chart to page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,4 @@ vegaEmbed('#vis6', 'specs/06_symbolmap.json', {actions: false, renderer: 'svg'})
 vegaEmbed('#vis7', 'specs/07_heatmap.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis8', 'specs/08_spikemap.json', {actions: false, renderer: 'svg'});
 vegaEmbed('#vis9', 'specs/09_slope.json', {actions: false, renderer: 'svg'});
+vegaEmbed('#vis10', 'specs/10_ridgeline.json', {actions: false, renderer: 'svg'});
